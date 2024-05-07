284b007a93da54935363a1eff24308f3cfc7b936
Liam Wright <mail@liam-wright.co.uk>
2024-05-07 16:10:40 +0100

    Downgrade to .NET Core to prevent BindingBootstrapper errors

--- src/SFA.DAS.Payments.AcceptanceTests.Core/Infrastructure/TestsConfiguration.cs
+++ src/SFA.DAS.Payments.AcceptanceTests.Core/Infrastructure/TestsConfiguration.cs
@@ -1,21 +1,91 @@
 ﻿using System;
-using System.Configuration;
+using Microsoft.Extensions.Configuration;
 
 namespace SFA.DAS.Payments.AcceptanceTests.Core.Infrastructure
 {
-    public class TestsConfiguration
+    public interface ITestsConfiguration
     {
-        public string AcceptanceTestsEndpointName => GetAppSetting("EndpointName");
-        public string StorageConnectionString => GetConnectionString("StorageConnectionString");
-        public string ServiceBusConnectionString => GetConnectionString("ServiceBusConnectionString");
-        public string DasServiceBusConnectionString => GetConnectionString("DASServiceBusConnectionString");
-        public string PaymentsConnectionString => GetConnectionString("PaymentsConnectionString");
-        public bool ValidateDcAndDasServices => bool.Parse(ConfigurationManager.AppSettings["ValidateDcAndDasServices"] ?? "false");
-        public TimeSpan TimeToWait => TimeSpan.Parse(ConfigurationManager.AppSettings["TimeToWait"] ?? "00:00:30");
-        public TimeSpan TimeToWaitForUnexpected => TimeSpan.Parse(ConfigurationManager.AppSettings["TimeToWaitForUnexpected"] ?? "00:00:30");
-        public TimeSpan TimeToPause => TimeSpan.Parse(ConfigurationManager.AppSettings["TimeToPause"] ?? "00:00:05");
-        public TimeSpan DefaultMessageTimeToLive => TimeSpan.Parse(ConfigurationManager.AppSettings["DefaultMessageTimeToLive"] ?? "00:20:00");
-        public string GetAppSetting(string keyName) => ConfigurationManager.AppSettings[keyName] ?? throw new InvalidOperationException($"{keyName} not found in app settings.");
-        public string GetConnectionString(string name) => ConfigurationManager.ConnectionStrings[name].ConnectionString ?? throw new InvalidOperationException($"{name} not found in connection strings.");
+        string AcceptanceTestsEndpointName { get; }
+        string StorageConnectionString { get; }
+        string ServiceBusConnectionString { get; }
+        string DasServiceBusConnectionString { get; }
+        string PaymentsConnectionString { get; }
+        string Environment { get; }
+        bool ValidateDcAndDasServices { get; }
+        TimeSpan TimeToWait { get; }
+        TimeSpan TimeToWaitForUnexpected { get; }
+        TimeSpan TimeToPause { get; }
+        TimeSpan DefaultMessageTimeToLive { get; }
+        string GetAppSetting(string keyName);
+        string GetConnectionString(string name);
+
+    }
+    public class TestsConfiguration : ITestsConfiguration
+    {
+        private static IConfigurationRoot _config;
+
+        public static readonly string EndpointNameAppSetting = "EndpointName";
+        public static readonly string StorageConnectionStringAppSetting = "StorageConnectionString";
+        public static readonly string ServiceBusConnectionStringAppSetting = "ServiceBusConnectionString";
+        public static readonly string DasServiceBusConnectionStringAppSetting = "DASServiceBusConnectionString";
+        public static readonly string EnvironmentAppSetting = "Environment";
+        public static readonly string PaymentsConnectionStringAppSetting = "PaymentsConnectionString";
+        public static readonly string ValidateDcAndDasServicesAppSetting = "ValidateDcAndDasServices";
+        public static readonly string TimeToWaitAppSetting = "TimeToWait";
+        public static readonly string TimeToWaitForUnexpectedAppSetting = "TimeToWaitForUnexpected";
+        public static readonly string TimeToPauseAppSetting = "TimeToPause";
+        public static readonly string DefaultMessageTimeToLiveAppSetting = "DefaultMessageTimeToLive";
+
+        public TestsConfiguration()
+        {
+            _config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true).Build();
+
+            if (Environment == "DEVELOPMENT" || Environment == "LOCAL")
+            {
+                _config = new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.Development.json", optional: false, reloadOnChange: true).Build();
+            }
+        }
+
+        public string AcceptanceTestsEndpointName => GetAppSetting(EndpointNameAppSetting) ??
+                                                     throw new InvalidOperationException($"{EndpointNameAppSetting} not found in app settings.");
+        public string StorageConnectionString => GetConnectionString(StorageConnectionStringAppSetting);
+        public string ServiceBusConnectionString => GetConnectionString(ServiceBusConnectionStringAppSetting);
+        public string DasServiceBusConnectionString => GetConnectionString(DasServiceBusConnectionStringAppSetting);
+        public string PaymentsConnectionString => GetConnectionString(PaymentsConnectionStringAppSetting);
+        public string Environment => GetAppSetting(EnvironmentAppSetting) ??
+                                     throw new InvalidOperationException($"{EnvironmentAppSetting} not found in app settings.");
+
+        public bool ValidateDcAndDasServices =>
+            bool.Parse(GetAppSetting(ValidateDcAndDasServicesAppSetting) ?? "false");
+
+        public TimeSpan TimeToWait =>
+            TimeSpan.Parse(GetAppSetting(TimeToWaitAppSetting) ?? "00:00:30");
+
+        public TimeSpan TimeToWaitForUnexpected =>
+            TimeSpan.Parse(GetAppSetting(TimeToWaitForUnexpectedAppSetting) ?? "00:00:30");
+
+        public TimeSpan TimeToPause =>
+            TimeSpan.Parse(GetAppSetting(TimeToPauseAppSetting) ?? "00:00:05");
+
+        public TimeSpan DefaultMessageTimeToLive =>
+            TimeSpan.Parse(GetAppSetting(DefaultMessageTimeToLiveAppSetting) ?? "00:20:00");
+
+        private static string AppSettingFormatter(string appSettingName)
+        {
+            return $"AppSettings:{appSettingName}";
+        }
+
+        public string GetAppSetting(string keyName)
+        {
+            return _config.GetSection(AppSettingFormatter(keyName)).Value;
+        }
+
+        public string GetConnectionString(string name)
+        {
+            return _config.GetConnectionString(name) ??
+                   throw new InvalidOperationException($"{name} not found in connection strings.");
+        }
     }
 }
